569f6bf5af6e4e721be088f30f63fa1b4ee493e4
Rui Mendonça <ruiagmendonca@gmail.com>
2024-07-24 00:50:14 +0100

    Swagger upgrade

--- src/bl-api/src/main.ts
+++ src/bl-api/src/main.ts
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
@@ -11,6 +10,7 @@ dotenv.config();
 
 async function bootstrap() {
   const apiPort = process.env.BL_API_PORT || 8080;
+  const authPort = process.env.AUTH_API_PORT || 18080;
   const swaggerPort = process.env.SWAGGER_PORT || 8081;
 
   try {
@@ -18,11 +18,29 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     app.use(cookieParser());
     app.enableCors();
-    setupSwagger(app);
     await app.listen(apiPort, () => {
-      const address = `http://localhost:${apiPort}`;
-      Logger.log(`API server is running on ${address}`);
-      Logger.log(`Swagger is available on ${address}/api/docs`);
+      const addressBL = `http://localhost:${apiPort}`;
+      Logger.log(`API server is running on ${addressBL}`);
+    });
+
+    // Start the auth server
+    const authApp = await NestFactory.create(AppModule, { cors: true });
+    authApp.use(cookieParser());
+    authApp.enableCors();
+    await authApp.listen(authPort, () => {
+      const authAddress = `http://localhost:${authPort}`;
+      Logger.log(`Auth server is running on ${authAddress}`);
+    });
+
+    // Start the Swagger server
+    const swaggerApp = await NestFactory.create(AppModule, { cors: true });
+    swaggerApp.use(cookieParser());
+    swaggerApp.enableCors();
+    setupSwagger(swaggerApp);
+    await swaggerApp.listen(swaggerPort, () => {
+      const addressSwagger = `http://localhost:${swaggerPort}`;
+      Logger.log(`Swagger server is running on ${addressSwagger}`);
+      Logger.log(`Swagger is available on ${addressSwagger}/api/docs`);
     });
   } catch (err) {
     Logger.error('Error starting server:', err);
--- src/bl-api/src/swagger.ts
+++ src/bl-api/src/swagger.ts
@@ -1,14 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
-  const config = new DocumentBuilder()
+  const options = new DocumentBuilder()
     .setTitle('API Documentation')
-    .setDescription('API description')
+    .setDescription('API documentation for the project')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/docs', app, document);
 }
